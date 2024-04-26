ab8c733b2b8b8d40185585080173dfb3e511d1d0
Arnel Gwen Nuqui <127367748+Gwenn01@users.noreply.github.com>
2024-04-26 15:36:45 +0800

    Add files via upload

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,16 @@
+
+function generatePassword(){
+    const vale = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const len = 16;
+    let password = "";
+    for(let i = 0; i < len; i++){
+        let random = Math.floor(Math.random() * vale.length);
+        password += vale[random];
+    }
+    return password
+}
+let passEl = document.getElementById('password');
+let btnGen = document.getElementById('generate');
+btnGen.addEventListener('click', () => {
+    passEl.value = generatePassword();
+})
